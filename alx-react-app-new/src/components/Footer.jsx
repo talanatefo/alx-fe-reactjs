ddeaa471bf531af5c795e80435aaafe22b633323
talana tefo <talanatefo@gmail.com>
2025-03-01 21:27:17 +0200

    added new folder

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ background: "indigo" }}>
-      <p style={{ color: "whitesmoke" }}>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
