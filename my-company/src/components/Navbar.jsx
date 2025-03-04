22cbc4ecb4055da5b0c685e3aa27c00ca560c922
talana tefo <talanatefo@gmail.com>
2025-03-04 21:10:48 +0200

    added navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <div>
+      <div
+        style={{
+          backgroundColor: "PINK",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <Link to="/">Fynbakes</Link>
-      </div>
-
-      <div>
         <Link to="/">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Services">Services</Link>
