77a1fafb3125ea65554864b255389653ff927e53
talana tefo <talanatefo@gmail.com>
2025-03-06 16:22:33 +0200

    added context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,16 @@
-import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </>
+    <div>
+      <p> This is our app page</p>
+      <UserContext.Provider value={userData}>
+        <ProfilePage userData={userData} />
+      </UserContext.Provider>
+    </div>
   );
 }
 
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,15 @@
+import { userContext } from "../App.jsx";
+import { UserContext } from "./UserContext.js";
+import { UseContext } from "react";
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
