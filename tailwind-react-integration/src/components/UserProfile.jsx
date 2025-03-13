49d9e9e9955fabd154def6ca38f3920cd65a4589
talana tefo <talanatefo@gmail.com>
2025-03-13 19:11:38 +0200

    added responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg ">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xs  ">
       <img
-        className="rounded-full w-36 h-36 mx-auto bg-gray-500"
+        className="rounded-full w-24 h-24 md:h-36 md:w-36  sm:w-24 sm:h-24  mx-auto bg-gray-500 "
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-blue-800 my-4" sm:text-lg md:text-xl>
+        John Doe
+      </h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
