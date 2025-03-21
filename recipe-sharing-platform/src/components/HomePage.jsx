f0ae313989db1b63ebfea68ad919f46a46721292
talana tefo <talanatefo@gmail.com>
2025-03-21 16:20:08 +0200

    updated responsiveness

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,7 +11,7 @@ function HomePage() {
   return (
     <div className=" px-2px">
       <h1 className="text-xl mx-auto">HomePage</h1>
-      <div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4 w-196 h-196 px-5px mx-10px">
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-2 gap-4 w-196 h-196 px-5px mx-10px">
         {recipes.map((recipe) => (
           <div key={recipe.id}>
             <div className="rounded-md bg-yellow-300 px-2 py-2 border-2 hover:bg-green-300 shadow-lg shadow-pink-500/50 ">
